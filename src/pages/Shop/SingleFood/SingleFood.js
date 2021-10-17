01484af361df693163cb5ef67c10ce979509ff73
chandan-mandi <itscoderchandan@gmail.com>
2021-10-17 20:34:48 +0530

    try

--- src/pages/Shop/SingleFood/SingleFood.js
+++ src/pages/Shop/SingleFood/SingleFood.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
-import useMeal from '../../../hooks/useMeal';
-
 
 const SingleFood = () => {
     const { mealId } = useParams();
